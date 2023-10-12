45f55e2f5b5ff623ca19a485633743364e3481e7
Paweł Śleziona <pawel.sleziona@gmail.com>
2023-10-12 21:12:55 +0200

    init

---
+++ backend/animedix/src/main/java/com/psleziona/animedix/AnimedixApplication.java
@@ -0,0 +1,13 @@
+package com.psleziona.animedix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimedixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimedixApplication.class, args);
+	}
+
+}
